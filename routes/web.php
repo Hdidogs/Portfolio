f20d3609f1f57684511f7ffa62da43f00ae79951
Hdidogs <hdidogs.pro@gmail.com>
2025-03-09 23:18:08 +0100

    Feat: all site

--- routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\CVController;
-use App\Http\Controllers\PortfolioController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', [PortfolioController::class, 'index']);
-Route::get('/download-cv', [CVController::class, 'download'])->name('download-cv');
